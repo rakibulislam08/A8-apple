785e84a7f00d27395d462134bd90deb0fbbbca85
Rakibul Islam <mdraislam56@gmail.com>
2026-09-19 19:29:14 +0600

    products api data update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { NEXT_ACTION_REVALIDATED_HEADER } from "next/dist/client/components/app-router-headers";
+// import { NEXT_ACTION_REVALIDATED_HEADER } from "next/dist/client/components/app-router-headers";
 
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      
     >
       <body>
         <Navbar/>
--- src/components/shared/TopApples.jsx
+++ src/components/shared/TopApples.jsx
@@ -1,9 +1,8 @@
 
+
+
 const TopApples =async () => {
-    const res = await fetch('https://a8-apple-tybd.vercel.app/products.json');
-    const info =await res.json();
-    console.log(info);
-     
+
 
     return (
         <div className="container mx-auto">
